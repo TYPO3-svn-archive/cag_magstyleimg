b381858f45251de5484b46d1bdce39d7b94eecd2
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-01 22:05:33 +0000

    1.0.0 released to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cag_magstyleimg/trunk@17369 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "cag_magstyleimg"
 #
-# Auto generated 21-06-2008 09:41
+# Auto generated 01-03-2009 23:02
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Implements a new content element "Magazine Images" with very nice looking automatic image layouts for up to eight images.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.1.3',
+	'version' => '1.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:13:"CHANGELOG.txt";s:4:"0550";s:10:"README.txt";s:4:"0fab";s:8:"TODO.txt";s:4:"2fdc";s:23:"cag_magstyleimg_pi1.gif";s:4:"c764";s:26:"cag_magstyleimg_pi1__f.gif";s:4:"138b";s:26:"cag_magstyleimg_pi1__h.gif";s:4:"f99f";s:27:"cag_magstyleimg_pi1__ht.gif";s:4:"5c76";s:27:"cag_magstyleimg_pi1__hu.gif";s:4:"46e1";s:26:"cag_magstyleimg_pi1__t.gif";s:4:"8d88";s:27:"cag_magstyleimg_pi1__tu.gif";s:4:"8460";s:26:"cag_magstyleimg_pi1__u.gif";s:4:"fb68";s:10:"ce_wiz.gif";s:4:"828f";s:21:"ext_conf_template.txt";s:4:"c409";s:12:"ext_icon.gif";s:4:"3d1c";s:17:"ext_localconf.php";s:4:"168b";s:14:"ext_tables.php";s:4:"dbb5";s:14:"ext_tables.sql";s:4:"1819";s:15:"flexform_ds.xml";s:4:"afb4";s:13:"locallang.xml";s:4:"446f";s:16:"locallang_db.xml";s:4:"36de";s:14:"doc/manual.sxw";s:4:"7266";s:19:"doc/wizard_form.dat";s:4:"c58b";s:20:"doc/wizard_form.html";s:4:"1fa3";s:22:"original/image.php.txt";s:4:"fb8c";s:33:"original/magazinelayout.class.txt";s:4:"aec5";s:37:"pi1/class.tx_cagmagstyleimg_hooks.php";s:4:"daeb";s:35:"pi1/class.tx_cagmagstyleimg_pi1.php";s:4:"061d";s:43:"pi1/class.tx_cagmagstyleimg_pi1_wizicon.php";s:4:"c88b";s:30:"pi1/class.ux_tx_cms_layout.php";s:4:"429a";s:20:"static/constants.txt";s:4:"802b";s:16:"static/setup.txt";s:4:"48a6";}',
+	'_md5_values_when_last_written' => 'a:31:{s:13:"CHANGELOG.txt";s:4:"dccd";s:10:"README.txt";s:4:"c021";s:8:"TODO.txt";s:4:"2199";s:23:"cag_magstyleimg_pi1.gif";s:4:"c764";s:26:"cag_magstyleimg_pi1__f.gif";s:4:"138b";s:26:"cag_magstyleimg_pi1__h.gif";s:4:"f99f";s:27:"cag_magstyleimg_pi1__ht.gif";s:4:"5c76";s:27:"cag_magstyleimg_pi1__hu.gif";s:4:"46e1";s:26:"cag_magstyleimg_pi1__t.gif";s:4:"8d88";s:27:"cag_magstyleimg_pi1__tu.gif";s:4:"8460";s:26:"cag_magstyleimg_pi1__u.gif";s:4:"fb68";s:10:"ce_wiz.gif";s:4:"828f";s:21:"ext_conf_template.txt";s:4:"a73c";s:12:"ext_icon.gif";s:4:"3d1c";s:17:"ext_localconf.php";s:4:"168b";s:14:"ext_tables.php";s:4:"2515";s:14:"ext_tables.sql";s:4:"1819";s:15:"flexform_ds.xml";s:4:"afb4";s:13:"locallang.xml";s:4:"446f";s:16:"locallang_db.xml";s:4:"7174";s:14:"doc/manual.sxw";s:4:"73f1";s:19:"doc/wizard_form.dat";s:4:"c58b";s:20:"doc/wizard_form.html";s:4:"1fa3";s:22:"original/image.php.txt";s:4:"fb8c";s:33:"original/magazinelayout.class.txt";s:4:"aec5";s:37:"pi1/class.tx_cagmagstyleimg_hooks.php";s:4:"c9fe";s:35:"pi1/class.tx_cagmagstyleimg_pi1.php";s:4:"2e3f";s:43:"pi1/class.tx_cagmagstyleimg_pi1_wizicon.php";s:4:"a07c";s:30:"pi1/class.ux_tx_cms_layout.php";s:4:"0ed5";s:20:"static/constants.txt";s:4:"802b";s:16:"static/setup.txt";s:4:"48a6";}',
+	'suggests' => array(
+	),
 );
 
 ?>
